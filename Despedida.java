955e30214e6ac40938b94fb30261a8a1fe9ef4e2
LJunLL <ljunll0000@gmail.com>
2024-11-22 16:40:13 +0100

    Cambio realizado

--- Despedida.java
+++ Despedida.java
@@ -5,7 +5,7 @@ public class Despedida {
     System.out.print("Dime tu nombre: ");
     String nombre = scan.nextLine();
 
-    System.out.println("Hasta luego " + nombre);
+    System.out.println("Hasta luego " + nombre + " .Espero verte más a menudo.");
     scan.close();
     }
 }
